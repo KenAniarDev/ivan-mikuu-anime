39f5f813799750080031399a2ecc8e50698da98b
Nestor Kenneth Tesorio Aniar <68641803+KenAniarDev@users.noreply.github.com>
2022-02-26 01:58:04 +0800

    Delete server directory

--- server/models/Login.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const LoginSchema = new mongoose.Schema({
-   username: String,
-   password: String,
-});
-
-const Login = mongoose.model("login", LoginSchema);
-module.exports = Login;
